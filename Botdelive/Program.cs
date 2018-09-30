e5e25f6ff581dde6367e55818c785da40e98f7aa
Farid Babayev <farid.b@code.edu.az>
2018-09-30 05:53:39 +0400

    inital commit

--- Botdelive/Program.cs
+++ Botdelive/Program.cs
@@ -10,16 +10,7 @@ namespace Botdelive
     {
         static void Main(string[] args)
         {
-            string appId = "B1QMo76F7";
-            string secret = "oPsNmyIvO6QgDYlgSyOjrCLO3OU-Wa4YNgfZ4UFf";
-            string accessCode = "SJI8oXaFm";
-            string userId = "xPdDzngBRwm8CGkSj4I9Abi-2gYNXz6uO0jOYq43";
-
-            Botdelive bt = new Botdelive(appId,secret);
-            //Console.WriteLine(bt.Verify(accessCode));
-            //Console.WriteLine(bt.Auth(userId));
-            Console.WriteLine(bt.Push(userId,"So Cool :) "));
-            Console.ReadLine();
+            Console.WriteLine("Hello :)");
         }
     }
 }
